e5e0b340e12116ed7e622ec73c67809063833bba
timeofneon <timeofneon@gmail.com>
2020-10-30 21:47:08 +0100

    fix(#9): clean-up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 /* eslint-env node */
+const Dotenv = require("dotenv-webpack");
 const path = require("path");
 const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
 
 const NODE_ENV = process.env.NODE_ENV;
 
@@ -95,22 +94,12 @@ module.exports = (/* env = {} */) => {
     },
 
     plugins: [
-      new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
-      }),
+      new Dotenv(),
 
       new webpack.optimize.MinChunkSizePlugin({
         minChunkSize: 20000,
       }),
       new CopyWebpackPlugin([{ from: "public" }]),
-
-      // TODO: resolve warning
-      // Or: To strip all locales except “en”, “es-us” and “ru”
-      // (“en” is built into Moment and can’t be removed)
-      new MomentLocalesPlugin({
-        localesToKeep: ["es-us", "ru", "uk", "cs"],
-      }),
-      new Dotenv(),
     ],
 
     devServer: {
@@ -118,14 +107,6 @@ module.exports = (/* env = {} */) => {
       host: "::", // Listens on all IPv4 and IPv6 interfaces
       port: 3000,
       historyApiFallback: true,
-
-      proxy: {
-        "/graphql": {
-          target: "https://body-monitor-be.herokuapp.com/v1/graphql",
-          secure: false,
-          changeOrigin: true,
-        },
-      },
     },
   };
 };
