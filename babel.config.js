956a4714be2cf56d88744e6679859e0536202e70
KseniiaChumachenko <kseniia.chumachenko@pleo.io>
2022-04-13 11:27:52 +0200

    chore(53): babel experiment 3

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,6 @@ module.exports = (api) => {
   const isTest = api.env("test");
 
   return {
-    plugins: [
-      "relay",
-      "macros",
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-optional-chaining",
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-    ],
     presets: [
       [
         "@babel/env",
@@ -21,6 +13,13 @@ module.exports = (api) => {
       ["@lingui/babel-preset-react"],
       ["@babel/typescript"],
     ],
-
+    plugins: [
+      ["relay"],
+      "macros",
+      "@babel/plugin-syntax-dynamic-import",
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+    ],
   };
 };
