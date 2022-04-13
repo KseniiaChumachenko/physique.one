ee4e7b647cd51ba23cc4217dd24806ccd75a8cc7
KseniiaChumachenko <kseniia.chumachenko@pleo.io>
2022-04-13 19:59:49 +0200

    chore(53): experiment 2 - change relay config

--- relay.config.js
+++ relay.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // ...
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
-  src: "./src",
+  // src: "./src",
   schema: "./src/graphql/schema.graphql",
   exclude: ["**/node_modules/**", "**/__mocks__/**"],
   customScalars: {
