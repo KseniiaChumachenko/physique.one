8ad723793bf17581ea65c97ddf1be8a5a1c4e1fa
KseniiaChumachenko <kseniia.chumachenko@pleo.io>
2022-04-13 19:27:21 +0200

    chore(53): experiment - userQuery type any

--- src/api-hooks/user/index.tsx
+++ src/api-hooks/user/index.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import { useQueryLoader, usePreloadedQuery, PreloadedQuery } from "react-relay";
 import { UserQuery as UserQueryDocument } from "./UserQuery";
-import {
-  UserQuery,
-  UserQueryVariables,
-} from "./__generated__/UserQuery.graphql";
+// import {
+//   UserQuery,
+//   UserQueryVariables,
+// } from "./__generated__/UserQuery.graphql";
 
 //export * from "./__generated__/UserQuery.graphql";
+interface UserQueryVariables {
+
+}
+
+type UserQuery = any
 
 export const useUser = (v: UserQueryVariables) => {
   const [queryReference, loadQuery] = useQueryLoader<UserQuery>(
